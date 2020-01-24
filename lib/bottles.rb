a527acdaec483eea1319702c92b5926b4928f583
MishalAfridi <mishal94.ma@gmail.com>
2020-01-24 17:55:54 +0000

    change if to case to accurately reflect the conditions are more alike than different

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,13 @@
 class Bottles
   def verse(num)
-    if num == 2
+    case 
+    when num == 2
       "2 bottles of milk on the wall, 2 bottles of milk.\n" + 
       "Take one down and pass it around, 1 bottle of milk on the wall.\n"
-    elsif num == 1
+    when num == 1
       "1 bottle of milk on the wall, 1 bottle of milk.\n" + 
       "Take it down and pass it around, no more bottles of milk on the wall.\n"
-    elsif num == 0
+    when num == 0
       "No more bottles of milk on the wall, no more bottles of milk.\n" + 
       "Go to the store and buy some more, 99 bottles of milk on the wall.\n"
     else
