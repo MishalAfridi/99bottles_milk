fe1dbff09c35290fc0a620f87f8420801f7ef707
MishalAfridi <mishal94.ma@gmail.com>
2020-01-28 09:31:39 +0000

    extract data clump into methodto_s, and implicitly call on bottle_number

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,10 +11,12 @@ class Bottles
     bottle_number = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of milk on the wall, " + 
-    "#{bottle_number.quantity} #{bottle_number.container} of milk.\n" +
+    "#{bottle_number} ".capitalize + 
+    "of milk on the wall, " + 
+    "#{bottle_number} of milk.\n" +
     "#{bottle_number.action}, " + 
-    "#{next_bottle_number.quantity} #{next_bottle_number.container} of milk on the wall.\n"
+    "#{next_bottle_number} " + 
+    "of milk on the wall.\n"
   end
 end
 
@@ -22,6 +24,10 @@ class BottleNumber
   attr_reader :number
   def initialize(number)
     @number = number
+  end
+
+  def to_s
+    "#{quantity} #{container}"
   end
 
   def quantity
